99b39348258b2ccba1febfd8e1e6c72ba314ce39
Olegych76 <f-o-r-j-o-b@yandex.ru>
2025-01-09 18:49:04 +0300

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -11,13 +11,13 @@ async def admin() -> dict:
 
 @app.get("/user/{user_id}")
 async def get_user(user_id: Annotated[
-    int, Path(description="Enter User ID", example="1", minimum=1, maximum=100)]) -> dict:
+    int, Path(gt=0, le=100, description="Enter User ID", example="1", minimum=1, maximum=100)]) -> dict:
     return {"Вы вошли как пользователь №": user_id}
 
 
 @app.get("/user/{username}/{age}")
 async def user_info(username: Annotated[
-    str, Path(description="Enter username", example="UrbanUser", minLength=5, maxLength=20)],
+    str, Path(description="Enter username", example="UrbanUser", min_length=5, max_length=20)],
                     age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> dict:
     return {"message": f"Информация о пользователе. Имя: '{username}', Возраст: {age}."}
 
